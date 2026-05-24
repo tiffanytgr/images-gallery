4e1ae8b662201e1b9ed9e7952998516ba1a3f525
tiffanytgr <tiffanytan.gr@gmail.com>
2026-05-24 18:09:31 +0800

    initial header component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,12 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './components/Header';
+
 
 const App = () => {
   return (
-    <div className="App">
-      <h1>My Image Gallery</h1>
-    </div>
+      <div>
+        <Header title="Images Gallery" />
+      </div>
   );
 }
 
